0df5f5e53c06acd648744cd066e91bccfc7f7eba
KeroCore <51845886+KeroCore@users.noreply.github.com>
2022-08-26 19:56:25 -0500

    Update mod.js
    changed the name and stuff

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Wack Tree",
+	id: "wackwackwackwackwackwackwack",
+	author: "Kero",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
